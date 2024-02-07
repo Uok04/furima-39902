619db58888cd1061622dd93d44dc64e449e31441
Uok04 <usaya382@gmail.com>
2024-02-07 16:39:21 +0900

    商品詳細機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
-  has_one :order
+  # has_one :order
 
   validates :item_name, presence: true
   validates :item_name, length: { maximum: 40, message: 'は40文字以下で入力してください' }
